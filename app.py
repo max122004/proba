12d57f0423bc07c3b3909e8a77f04895324d6224
max122004! <boldirevmax10@gmail.com>
2023-03-02 22:23:00 +0300

    proba

---
+++ app.py
@@ -0,0 +1,52 @@
+from flask import Flask, render_template, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+
+from cloudipsp import Api, checkout
+
+app = Flask(__name__, template_folder='templates')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///shop.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATION'] = False
+db = SQLAlchemy(app)
+
+class Item(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    price = db.Column(db.Integer, nullable=False)
+    isActive = db.Column(db.Boolean, default=True)
+    text = db.Column(db.Text, nullable=False)
+
+    def __repr__(self):
+        return f'Запись: {self.title}'
+
+
+@app.route('/')
+def index():
+    items = Item.query.order_by(Item.price).all()
+    return render_template('index.html', data=items)
+
+@app.route('/buy/<int:mid>')
+def buy(mid):
+
+    return render_template()
+
+@app.route('/create', methods=['POST', 'GET'])
+def create():
+    if request.method == 'POST':
+        title = request.form['title']
+        price = request.form['price']
+
+        item = Item(title=title, price=price)
+
+        try:
+            db.session(item)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'Получилась ошибка'
+    else:
+        return render_template('create.html')
+
+
+
+if __name__ == '__main__':
+    app.run()
